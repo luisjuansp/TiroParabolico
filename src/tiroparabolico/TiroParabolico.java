cadfed9f7f58d005df591c02d07a50b3a973d16d
Alfredo Hinojosa <alfhh4@gmail.com>
2014-02-25 14:55:06 -0600

    Se agrego la imagen de fondo y el metodo paint, ESTE DEBE SER CAMBIADO POR PAINT1

--- src/tiroparabolico/TiroParabolico.java
+++ src/tiroparabolico/TiroParabolico.java
@@ -7,20 +7,33 @@ package tiroparabolico;
 
 import javax.swing.JFrame;
 import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Graphics;
 
 public class TiroParabolico extends JFrame {
 
     private Image background; // Imagen de fondo de JFrame <-- Agregar Imagen
     
     /**
-     * Constructor.
+     * Constructor
      * Se inicializan las variables
      */
     public TiroParabolico() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1200, 750);
+        setSize(1008, 758);
         setTitle("NBA Series!");
         
+        background = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("images/nba.jpg"));
+        
+    }
+    
+    /**
+     * ESTE METODO DEBE SER CAMBIADO POR PAINT1
+     * Este metodo
+     * @param g objeto grafico
+     */
+    public void paint(Graphics g){
+        g.drawImage(background, 0, 0, this);
     }
     
     public static void main(String[] args) {
